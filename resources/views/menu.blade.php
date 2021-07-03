71ee4be21dfe4723310d0ae1cb8ded3bb69371b5
sertekmm <sertek.mm@gmail.com>
2021-07-02 23:27:38 -0500

    Initial commit

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Security 911 Trujillo</title>
+    <link rel="stylesheet" href="css/estilos.css">
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+    <header class="header" id="inicio">
+        <img src="img/hamburguesa.svg" all="" class="hamburguesa">
+        <nav class="menu-navegacion">
+                <a href="#inicio">INICIO</a>
+                <a href="#quienesomoso">QUIENES SOMOS</a>
+				<a href="#servicio">SERVICIOS</a>
+                <a href="#galeria">GALERIA</a>
+				<a href="#contactanos">CONTACTANOS</a>
+        </nav>
+        <div class="contenedor head">
+            <h1 class="titulo">SECURITY CONCERTS & VENUES</h1>
+            <p class="copy">Servicio altamente especializado en seguridad personal y vigilancia</p>
+            <div class="trujillo-boton">
+            <a href="a" class="trujillo">BIENVENIDO</a>
+        </div>
+        </div>
+    </header>
+    
+    <main>
+        <section class="contenedor" id="servicio">
+            <h2 class="subtitulo">SERVICIOS</h2>
+            <div class="contenedor-servicio">
+                <img src="img/servicios.svg" all="">
+                <div class="checklist-servicio">
+                    <div class="servicio">
+                        <h3 class="n-service"><span class="number">1</span>SHOW BUSINESS SERVICES</h3>
+                        <p> Seguridad en Conciertos y Espectáculos.
+                        Seguridad de Artistas y Dignatarios.
+                        Seguridad y Protección de Instalaciones.
+                        Vigilancia de certámenes, convenciones y ferias.
+                        Servicios de Unidad de Rescate.</p>
+                    </div>
+                        <h3 class="n-service"><span class="number">2</span>CORPORATE SECURITY</h3>
+                        <p> Seguridad en Conciertos y Espectáculos.
+                        Seguridad de Artistas y Dignatarios.
+                        Seguridad y Protección de Instalaciones.
+                        Vigilancia de certámenes, convenciones y ferias.
+                        Servicios de Unidad de Rescate.</p>
+                    </div>
+                        <h3 class="n-service"><span class="number">3</span>SECURITY CONCERTS & VENUES</h3>
+                        <p> Seguridad en Conciertos y Espectáculos.
+                        Seguridad de Artistas y Dignatarios.
+                        Seguridad y Protección de Instalaciones.
+                        Vigilancia de certámenes, convenciones y ferias.
+                        Servicios de Unidad de Rescate.</p>
+                </div>
+        </section>
+        
+        <section class="galeria" id="portafolio">
+            <div class="contenedor">
+                <h2 class="subtitulo">GALERIA</h2>
+                <div class="contenedor-galeria">
+                    <img src="img/corporate0.jpg" all="" class="img-galeria">
+                    <img src="img/corporate2.jpg" all="" class="img-galeria">
+                    <img src="img/corporate3.jpg" all="" class="img-galeria">
+                    <img src="img/corporate4.jpg" all="" class="img-galeria">
+                    <img src="img/corporate5.jpg" all="" class="img-galeria">
+                    <img src="img/corporate6.jpg" all="" class="img-galeria">
+                    <img src="img/corporate7.jpg" all="" class="img-galeria">
+                    <img src="img/corporate8.jpg" all="" class="img-galeria">
+                    <img src="img/corporate9.jpg" all="" class="img-galeria">
+                </div>
+            </div>
+        </section>
+
+        <section class="imagen-light">
+            <img src="img/close.svg" alt="" class="close">
+            <img src="img/servicio.jpg" alt="" class="agregar-imagen">
+        </section>
+
+        <section class="contenedor" id="expertos">
+            <h2 class="subtitulo">EXPERTOS EN:</h2>
+            <section class="experts">
+                <div class="cont-experts">
+                    <img src="img/user.svg" alt="">
+                    <h3 class="n-expert">Analisis</h3>
+                </div>
+                <div class="cont-experts">
+                    <img src="img/seguridad2.svg" alt="">
+                    <h3 class="n-expert">Suficiencia</h3>
+                </div>
+                <div class="cont-experts">
+                    <img src="img/seguridad.svg" alt="">
+                    <h3 class="n-expert">Seguridad</h3>
+                </div>
+            </section>
+        </section>
+    </main>
+
+    <footer id="contacto">
+        <div class="contenedor footer-content">
+            <div class="contact-us">
+            <h2 class="brand"> Seguridad 911 – Base Tujillo</h2>
+            <p>Calle Cuba 191 Urb. El Recreo - T.: (044)287798 ANEXO 103 - Cel.924281039</p>
+            </div>
+        <div class="social-media">
+                <a href="./ " class="social-media-icon">
+                    <i class='bx bxl-facebook'></i>
+                </a>
+                <a href="./ " class="social-media-icon">
+                    <i class='bx bxl-instagram' ></i>
+                </a>
+                <a href="./ " class="social-media-icon">
+                    <i class='bx bxl-youtube' ></i>
+                 </a>
+            </div>
+        </div>
+        <div class="line"></div>
+    </footer>
+
+    <script src="js/menu.js"></script>
+    <script src="js/lightbox.js"></script>
+</body>
+</html>
